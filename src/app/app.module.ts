b8085087c15abfec2b43917a1e0574f471c50c6e
Fátima <gonzalezperezfa@gmail.com>
2020-10-02 22:06:05 -0500

    styled login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './components/login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+const rutas: Routes = [
+  { path: '', component: LoginComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(rutas),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
