aff224d7ec952472880c79e9106ca30f22bc8a44
Ivan Arredondo <a16310034@ceti.mx>
2020-10-05 18:41:15 -0500

    cambios en header: navbar y header unidos con nuestro logo, rutas hijas establecidas y pagina de estudiantes parcialmente funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { StudentsComponent } from './components/students/students.component';
+import { CitasComponent } from './components/citas/citas.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'inicio', component: DashboardComponent, children: [
+    {path: 'estudiantes', component: StudentsComponent},
+    {path: 'citas', component: CitasComponent},
+    {path: '**', redirectTo: 'estudiantes'},
+  ]},
+  {path: '**', redirectTo: ''}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,25 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { StudentsComponent } from './components/students/students.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavigationComponent } from './layout/navigation/navigation.component';
+import { CitasComponent } from './components/citas/citas.component';
 const rutas: Routes = [
   { path: '', component: LoginComponent },
 ];
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent,
+    StudentsComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavigationComponent,
+    CitasComponent
   ],
   imports: [
     BrowserModule,
