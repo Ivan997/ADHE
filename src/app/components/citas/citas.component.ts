2367c63254935f91a67333e7664b1d5326416898
Fátima <gonzalezperezfa@gmail.com>
2020-11-09 20:00:22 -0600

    mostrar fecha en español

--- src/app/components/citas/citas.component.ts
+++ src/app/components/citas/citas.component.ts
@@ -19,12 +19,12 @@ export class CitasComponent implements OnInit {
   dates = [];
   passDates = [];
 
-  constructor( private as: AlumnosService) { }
+  constructor(private as: AlumnosService) { }
 
   ngOnInit(): void {
 
 
-    let citx =  [];
+    let citx = [];
 
     let promesaCitas = new Promise((resolve) => {
       this.as.getCitas().subscribe(resp => {
@@ -33,55 +33,56 @@ export class CitasComponent implements OnInit {
       });
     }).then(() => {
       citx.forEach(index => {
-          // console.log('Buscando en index');
+        // console.log('Buscando en index');
 
-          const fecha = index.fecha.split('/');
-          const hora = index.hora.split(':');
+        const fecha = index.fecha.split('/');
+        const hora = index.hora.split(':');
 
-          const fec = new Date(parseInt(fecha[2]), parseInt(fecha[1]), parseInt(fecha[0]), parseInt(hora[0]), parseInt(hora[1]), 0);
+        const fec = new Date(parseInt(fecha[2]), parseInt(fecha[1]), parseInt(fecha[0]), parseInt(hora[0]), parseInt(hora[1]), 0);
 
-          const diaC = fec.getDate();
-          const mesC = fec.getMonth();
-          const anioC = fec.getFullYear();
+        const diaC = fec.getDate();
+        const mesC = fec.getMonth();
+        const anioC = fec.getFullYear();
 
-          const diaH = this.today.getDate();
-          const mesH = this.today.getMonth();
-          const anioH = this.today.getFullYear();
+        const diaH = this.today.getDate();
+        const mesH = this.today.getMonth();
+        const anioH = this.today.getFullYear();
 
-          // ya paso el mes, dia o año?
+        // ya paso el mes, dia o año?
 
-          const objCita = {
-            id: index.id,
-            area: index.area,
-            registro: index.registro,
-            nombre: index.nombre + ' ' + index.apellido,
-            encargado : index.encargado,
-            date: fec,
-            location: index.area,
-            notes: index.nota,
-            finished: index.finalizado,
-            photo: 'https://raw.githubusercontent.com/Ivan997/ADHE-img/master/' + index.registro + '.jpg'
-          };
+        const objCita = {
+          id: index.id,
+          area: index.area,
+          registro: index.registro,
+          nombre: index.nombre + ' ' + index.apellido,
+          encargado: index.encargado,
+          date: fec,
+          location: index.area,
+          notes: index.nota,
+          finished: index.finalizado,
+          photo: 'https://raw.githubusercontent.com/Ivan997/ADHE-img/master/' + index.registro + '.jpg'
+        };
+        console.log(objCita);
 
-          if ( anioC < anioH ){
-            this.passDates.push(objCita);
-            this.actualizarCitaPass(index);
-          }
-          else if ( anioC > anioH ){ this.dates.push(objCita); }
-          else if ( mesC > mesH){ this.dates.push(objCita); }
-          else if ( anioC === anioH && mesC === mesH  && diaC >= diaH){ this.dates.push(objCita); }
-          else {
-            this.passDates.push(objCita);
-            if ((index.asistencia && !index.finalizado) || !index.finalizado) { this.actualizarCitaPass(index); }
-          }
+        if (anioC < anioH) {
+          this.passDates.push(objCita);
+          this.actualizarCitaPass(index);
+        }
+        else if (anioC > anioH) { this.dates.push(objCita); }
+        else if (mesC > mesH) { this.dates.push(objCita); }
+        else if (anioC === anioH && mesC === mesH && diaC >= diaH) { this.dates.push(objCita); }
+        else {
+          this.passDates.push(objCita);
+          if ((index.asistencia && !index.finalizado) || !index.finalizado) { this.actualizarCitaPass(index); }
+        }
 
-          this.dates.sort((a, b) => {
-            return a.date.getTime() - b.date.getTime();
-          });
-          this.passDates.sort((a, b) => {
-            return a.date.getTime() - b.date.getTime();
-          });
+        this.dates.sort((a, b) => {
+          return a.date.getTime() - b.date.getTime();
         });
+        this.passDates.sort((a, b) => {
+          return a.date.getTime() - b.date.getTime();
+        });
+      });
 
       // console.log('this.dates');
       // console.log(this.dates);
@@ -91,9 +92,17 @@ export class CitasComponent implements OnInit {
 
   }
 
+  formatearFecha(fecha) {
+    let aux = new Date(fecha);
+    let hours = aux.getHours();
+    var ampm = hours >= 12 ? ' PM' : ' AM';
+    let format = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+    return aux.toLocaleDateString('es-ES', format) + ampm;
+  }
 
-  actualizarCitaPass(cita: CitasModel): any{
-    if (!cita.finalizado || cita.asistencia){
+
+  actualizarCitaPass(cita: CitasModel): any {
+    if (!cita.finalizado || cita.asistencia) {
       console.log('Actualizando');
       cita.finalizado = true;
       this.as.actualizarCita(cita).subscribe();
@@ -101,15 +110,15 @@ export class CitasComponent implements OnInit {
   }
 
 
-  finalizado(cita){
+  finalizado(cita) {
     this.dates.map(dat => {
-      if ( dat.date.getTime() === cita.date.getTime() ){
+      if (dat.date.getTime() === cita.date.getTime()) {
         dat.finished = true;
       }
     });
   }
 
-  comparar(cita): number{
+  comparar(cita): number {
     // retorna :
     // -1 si la fecha ya paso
     // 0 si es hoy
@@ -126,24 +135,24 @@ export class CitasComponent implements OnInit {
     const mesH = this.today.getMonth();
     const anioH = this.today.getFullYear();
 
-    if (anio < anioH || mes < mesH){
+    if (anio < anioH || mes < mesH) {
       this.pos = -1;
       this.finalizado(cita);
       return this.pos;
     }
 
     // ya paso el mes, dia o año?
-    if (anio > anioH || mes > mesH || dia > (diaH + 7)){
+    if (anio > anioH || mes > mesH || dia > (diaH + 7)) {
       this.pos = 2;
       return this.pos;
     }
 
-    if (dia < diaH){
+    if (dia < diaH) {
       this.pos = -1;
       this.finalizado(cita);
-    }else if (dia <= (diaH + 7) && dia > diaH){
+    } else if (dia <= (diaH + 7) && dia > diaH) {
       this.pos = 1;
-    }else {
+    } else {
       this.pos = 0;
     }
 
