72ddb7a932380596d9da6055da4f560a9532e945
Fátima <gonzalezperezfa@gmail.com>
2020-11-10 00:32:13 -0600

    picker

--- src/app/components/common/citas/formulario-citas/formulario-citas.component.ts
+++ src/app/components/common/citas/formulario-citas/formulario-citas.component.ts
@@ -3,7 +3,8 @@ import { FormGroup, FormBuilder, Validators, NgForm } from '@angular/forms';
 import { CitasModel } from '../../../../models/citas.model';
 import { AlumnosService } from '../../../../services/alumnos.service';
 import Swal from 'sweetalert2';
-
+import moment, { isMoment } from 'moment';
+moment.locale('es');
 
 @Component({
   selector: 'app-formulario-citas',
@@ -15,57 +16,63 @@ export class FormularioCitasComponent implements OnInit {
 
   forma: FormGroup;
 
-  fecha = new Date('12/21/2020');//MM/DD/AAAA HH:MM:SS
+  fecha = moment().format('yyyy-mm-dd')
 
   private cita = new CitasModel();
+  esFinDeSemana: boolean;
 
-  constructor( private fb: FormBuilder, private as: AlumnosService) {
+  constructor(private fb: FormBuilder, private as: AlumnosService) {
     this.crearFormulario();
     this.cargarDatos();
   }
 
-  get registroValido(){
-  return this.forma.get('registro').invalid && this.forma.get('registro').touched;
+  get registroValido() {
+    return this.forma.get('registro').invalid && this.forma.get('registro').touched;
   }
-  get nombreValido(){
-  return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
+  get nombreValido() {
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
   }
-  get apellidoValido(){
-  return this.forma.get('apellido').invalid && this.forma.get('apellido').touched;
+  get apellidoValido() {
+    return this.forma.get('apellido').invalid && this.forma.get('apellido').touched;
   }
-  get encargadoValido(){
-  return this.forma.get('encargado').invalid && this.forma.get('encargado').touched;
+  get encargadoValido() {
+    return this.forma.get('encargado').invalid && this.forma.get('encargado').touched;
   }
-  get fechaValido(){
-  return this.forma.get('fecha').invalid && this.forma.get('fecha').touched;
+  get fechaValido() {
+    return this.forma.get('fecha').invalid && this.forma.get('fecha').touched;
   }
-  get horaValido(){
-  return this.forma.get('hora').invalid && this.forma.get('hora').touched;
+  get horaValido() {
+    return this.forma.get('hora').invalid && this.forma.get('hora').touched;
   }
-  get areaValido(){
-  return this.forma.get('area').invalid && this.forma.get('area').touched;
+  get areaValido() {
+    return this.forma.get('area').invalid && this.forma.get('area').touched;
   }
 
   ngOnInit(): void {
     this.cita.id = '';
   }
 
-  crearFormulario(){
+  crearFormulario() {
 
     this.forma = this.fb.group({
-      registro   : ['', [Validators.required, Validators.minLength(4)]],
-      nombre     : ['', [Validators.required, Validators.minLength(3)]],
-      apellido   : ['', [Validators.required, Validators.minLength(4)]],
-      fecha      : ['', [Validators.required]],
-      hora       : ['', [Validators.required]],
-      encargado  : ['', [Validators.required, Validators.minLength(4)]],
-      area       : ['', [Validators.required, Validators.minLength(3)]],
-      nota       : ['']
+      registro: ['', [Validators.required, Validators.minLength(4)]],
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      apellido: ['', [Validators.required, Validators.minLength(4)]],
+      fecha: ['', [Validators.required]],
+      hora: ['', [Validators.required]],
+      encargado: ['', [Validators.required, Validators.minLength(4)]],
+      area: ['', [Validators.required, Validators.minLength(3)]],
+      nota: ['']
     });
 
   }
 
-  cargarDatos(){
+  validarFecha(fecha) {
+    let aux = moment(fecha.target.value).format("dddd");
+    this.esFinDeSemana = aux == "sábado" || aux == "domingo"
+  }
+
+  cargarDatos() {
 
     let nombreCompleto: string = this.as.student.nombre;
     let nombres = '';
@@ -73,38 +80,38 @@ export class FormularioCitasComponent implements OnInit {
     let apellido = '';
     let reg = '';
 
-    if (this.as !== undefined){
+    if (this.as !== undefined) {
       try {
         nombres = nombreCompleto.split(' ');
         nombre = nombres[0];
-        apellido = nombres[nombres.length - 1 ];
+        apellido = nombres[nombres.length - 1];
         reg = this.as.alumnoActual;
       } catch (error) {
         console.log(error);
       }
     }
 
-    if(reg === ''){
+    if (reg === '') {
       reg = '';
     }
 
     this.forma.setValue(
       {
-        registro  : reg,
-        nombre    : nombre,
-        apellido  : apellido,
-        encargado : '',
-        fecha     : this.fecha.getDate() + '/' + this.fecha.getMonth() + '/' + this.fecha.getFullYear() ,
-        hora      : '12:30',
-        area      : '',
-        nota      : ''
+        registro: reg,
+        nombre: nombre,
+        apellido: apellido,
+        encargado: '',
+        fecha: '',
+        hora: '12:30',
+        area: '',
+        nota: ''
       }
     );
   }
 
-  guardar(form: NgForm){
+  guardar(form: NgForm) {
 
-    if (form.invalid){
+    if (form.invalid) {
 
       Object.values(this.forma.controls).forEach(control => {
         control.markAsTouched();
@@ -121,7 +128,7 @@ export class FormularioCitasComponent implements OnInit {
     });
     Swal.showLoading();
 
-    if ( this.cita.id !== ''){
+    if (this.cita.id !== '') {
       Swal.fire({
         icon: 'error',
         title: 'Error',
@@ -142,7 +149,7 @@ export class FormularioCitasComponent implements OnInit {
     this.cita.finalizado = false;
     this.cita.asistencia = false;
 
-
+    this.cita.fecha = moment(this.cita.fecha).format('DD/MM/YYYY')
     console.log(this.cita);
 
     this.as.crearCita(this.cita).subscribe(resp => {
